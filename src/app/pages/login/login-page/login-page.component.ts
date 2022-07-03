4897677bc231010886eafeb82d723ac34f2b3f47
DESKTOP-VI6LSH9\NoraHajdu <hajdu.nora115@gmail.com>
2022-07-03 13:22:56 +0200

    No need to login when auth cookie is present

--- src/app/pages/login/login-page/login-page.component.ts
+++ src/app/pages/login/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private cookieService: CookieService
+    ) { }
 
   ngOnInit(): void {
+    const currentUser = this.cookieService.get('Auth');
+    if (currentUser) {
+        // authorised so return true
+        this.router.navigate(['contacts'], { });
+    }
   }
 
 }
